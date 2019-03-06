f9768d8c4daca9a6751cc0e087abb353d53ce122
Natalia <garcia.doe@gmail.com>
2019-03-06 00:33:49 -0800

    Add footer to all no matches

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import "./style/main.scss"
 
 // Import i18n so its bundled by webpack
 import './i18n';
+import Footer from './components/footer';
 
 //TODO: change suspense fallback to a loading indicator
 const MainComponent = () => (
@@ -16,7 +17,7 @@ const MainComponent = () => (
         <Router>
             <Switch>
                 <Route exact path="/:id" render={() => <App />} />
-                <Route render={() => <NoMatch />} />
+                <Route render={() => <><NoMatch /><Footer /></>} />
             </Switch>
         </Router>
     </React.Suspense>);
